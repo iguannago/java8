a039a640afd2811fc0efe5fb27dc5446daf40aff
David Crespo <davidcrespoarroyo@gmail.com>
2016-01-13 20:03:34 +0000

    Put to sleep a thread

--- java8/src/main/java/com/getinthecloud/app/Concurrency.java
+++ java8/src/main/java/com/getinthecloud/app/Concurrency.java
@@ -1,11 +1,37 @@
 package com.getinthecloud.app;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by davicres on 13/01/2016.
  */
 public final class Concurrency {
 
+
+
     public static void main(String[] args) {
+//        normalConcurrentTask();
+        concurrentTaskPutToSleep();
+    }
+
+    private static void concurrentTaskPutToSleep() {
+        Runnable runnable = () -> {
+            try {
+                String name = Thread.currentThread().getName();
+                System.out.println("Foo " + name);
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println("Bar " + name);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+    private static void normalConcurrentTask() {
         Runnable task = () -> {
             String threadName = Thread.currentThread().getName();
             System.out.println("Hello " + threadName);
